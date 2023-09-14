850e5683c2bc42b85b2c73779b50a64684e6663a
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-09-14 04:21:39 -0500

    Finished adding CRUD functions

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -20,8 +20,10 @@
         class="text-primary border df"
         width="20%"
       >
+        <!-- CREATE PRODUCT -->
         <v-card-item
           ><v-text-field
+            v-model="newProduct.upc"
             label="Enter UPC"
             variant="outlined"
             class="pa-2"
@@ -29,6 +31,7 @@
         ></v-card-item>
         <v-card-item
           ><v-text-field
+            v-model="newProduct.location"
             label="Enter Location"
             variant="outlined"
             class="pa-2"
@@ -36,14 +39,19 @@
         ></v-card-item>
         <v-container>
           <v-row class="justify-center pa-3">
-            <v-btn color="white" class="bg-primary justify-center" width="80%"
+            <v-btn
+              @click="createProduct()"
+              color="white"
+              class="bg-primary justify-center"
+              width="80%"
               >Create</v-btn
             >
           </v-row>
         </v-container>
       </v-card>
+      <!-- SEARCH PRODUCT -->
       <v-card
-        v-else
+        v-else-if="!update"
         title="Search for a Product"
         class="text-primary border df"
         width="20%"
@@ -56,16 +64,50 @@
             class="pa-2"
           ></v-text-field
         ></v-card-item>
-        <v-card-item
+        <v-card-item v-if="product"
           ><div>Location: {{ product.location }}</div></v-card-item
         >
+        <v-card-item v-if="product">
+          <v-btn @click="deleteProduct(product.upc)" color="red">
+            Delete
+          </v-btn>
+          <v-btn @click="toggleUpdate()" class="ml-5" color="green">
+            Update
+          </v-btn>
+        </v-card-item>
         <v-container>
           <v-row class="justify-center pa-3">
-            <v-btn @click="getProduct(upc)" color="white" class="bg-primary justify-center" width="80%"
+            <v-btn
+              @click="getProduct(upc)"
+              color="white"
+              class="bg-primary justify-center"
+              width="80%"
               >Search</v-btn
             >
           </v-row>
         </v-container>
+      </v-card>
+      <!-- UPDATE PRODUCT -->
+      <v-card
+        v-if="update"
+        title="Update Product"
+        class="text-primary border df"
+        width="20%"
+      >
+        <v-card-item
+          ><v-text-field
+            v-model="updateLocation"
+            label="Enter New Location"
+            variant="outlined"
+            class="pa-2"
+          ></v-text-field
+        ></v-card-item>
+        <v-card-item>
+          <v-btn @click="toggleCancel()" color="red"> cancel </v-btn>
+          <v-btn @click="updateProduct(product.upc)" class="ml-5" color="green">
+            Confirm Update
+          </v-btn>
+        </v-card-item>
       </v-card>
     </v-row>
     <v-row class="justify-center" v-for="product in products" :key="product.id">
@@ -80,13 +122,29 @@ import { ref, onMounted } from "vue";
 import axios from "axios";
 
 const isCreate = ref(true);
+const update = ref(false);
 const products = ref([]);
 const upc = ref("");
+const updateLocation = ref('')
 const product = ref("");
+const newProduct = ref({
+  upc: "",
+  location: "",
+});
 
 const toggleisCreate = () => {
   isCreate.value = !isCreate.value;
+  update.value = false;
 };
+
+const toggleUpdate = () => {
+  update.value = true;
+};
+
+const toggleCancel = () => {
+  update.value = false
+  updateLocation.value = ''
+}
 
 // Get All Products when app Mount
 onMounted(() => {
@@ -99,8 +157,31 @@ onMounted(() => {
 const getProduct = (upc) => {
   axios
     .get(`https://products-api-putj.onrender.com/products/${upc}`)
-    .then((response) => (product.value) = response.data.product);
+    .then((response) => (product.value = response.data.product));
 };
+
+// Create a new Product
+const createProduct = () => {
+  axios
+    .post("https://products-api-putj.onrender.com/products", newProduct.value)
+    .then((response) => console.log(response.data.product))
+};
+
+//Update a Product
+const updateProduct = (upc) => {
+  axios
+    .patch(`https://products-api-putj.onrender.com/products/${upc}`, {
+      location: updateLocation.value
+    })
+    .then(update.value = false);
+};
+// Delete a Product
+const deleteProduct = (upc) => {
+  axios
+    .delete(`https://products-api-putj.onrender.com/products/${upc}`)
+    .then((response) => (product.value = response.data.product));
+};
+
 </script>
 
 <style lang="scss" scoped></style>
